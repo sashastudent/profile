4f024d50f983c8bda10e0657b1793445cc431b1c
SashaBlank <sasha21.blank@gmail.com>
2020-05-23 17:38:49 +0300

    fixed skills and add instruction for game

--- src/app/left-container/skills-list/skills-list.service.ts
+++ src/app/left-container/skills-list/skills-list.service.ts
@@ -1,5 +1,5 @@
 export class SkillsListService{
 
-    skillsList :string[]=['C#','Angular','HTML5','CSS3','Bootstrap','JavaScript','jQuery','AJAX','SQL','ASP.NET MVC Framework ','ASP.Net Web API Framework']
+    skillsList :string[]=['Software Engineering', 'OOP','Agile','C#','Angular','HTML5','CSS3','Bootstrap','JavaScript','jQuery','AJAX','SQL','ASP.NET MVC Framework ','ASP.Net Web API Framework', 'ML/AI','JavaEE','Python','Azure']
 
 }
