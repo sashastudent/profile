116f70d3a5a7ca782c5efe2e9af55a13bd7dca66
SashaBlank <sasha21.blank@gmail.com>
2020-05-26 15:22:58 +0300

    add skills

--- src/app/left-container/skills-list/skills-list.service.ts
+++ src/app/left-container/skills-list/skills-list.service.ts
@@ -1,5 +1,5 @@
 export class SkillsListService{
 
-    skillsList :string[]=['Software Engineering', 'OOP','Agile','C#','Angular','HTML5','CSS3','Bootstrap','JavaScript','jQuery','AJAX','SQL','ASP.NET MVC Framework ','ASP.Net Web API Framework', 'ML/AI','JavaEE','Python','Azure']
+    skillsList :string[]=['Software Engineering', 'OOP','Agile','C#','Angular','TypeScript','HTML5','CSS3','Bootstrap','JavaScript','jQuery','AJAX','SQL','ASP.Net MVC Framework ','ASP.Net Web API Framework', 'ML/AI','JavaEE','Python','Azure']
 
 }
